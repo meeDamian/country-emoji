c4c8f53c50be314db6f2035071f3436b44d40aaf
Damian Mee <spam@meedamian.com>
2019-04-24 18:32:27 +0700

    remove dist from git

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _require = require('./lib.js'),
-    flag = _require.flag,
-    name = _require.name,
-    code = _require.code,
-    countries = _require.countries;
-
-module.exports = { flag: flag, name: name, code: code, countries: countries };
