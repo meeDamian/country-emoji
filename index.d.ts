72faf24b347dc55cacef787474a996c6f55d6fdc
marzeq <marzeqmarzeq@gmail.com>
2021-03-01 22:57:10 +0100

    Added TypeScript support

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+export interface ICountries {
+    [key: string]: [string, string];
+}
+
+
+declare module 'country-emoji' {
+    export const flag: (input: string) => string;
+  
+    export const code: (input: string) => string;
+  
+    export const name: (input: string) => string;
+
+    export const countries: ICountries;
+}
