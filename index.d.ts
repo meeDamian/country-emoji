45d00573509d754adf639354e4a91906eaba8126
marzeq <marzeqmarzeq@gmail.com>
2021-03-02 10:19:23 +0100

    Fix indentation and other formatting issues

--- index.d.ts
+++ index.d.ts
@@ -4,11 +4,8 @@ export interface ICountries {
 
 
 declare module 'country-emoji' {
-    export const flag: (input: string) => string;
-  
-    export const code: (input: string) => string;
-  
-    export const name: (input: string) => string;
-
-    export const countries: ICountries;
+	export const flag: (input: string) => string;
+	export const code: (input: string) => string;
+	export const name: (input: string) => string;
+	export const countries: ICountries;
 }
