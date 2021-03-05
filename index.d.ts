e6b959c754a0741cf2d536146cf8817728f2a329
Rodrigo <rodrigoleitao25@gmail.com>
2021-03-05 18:58:39 +0000

    Add types declaration and fix descriptions

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,30 @@
-export interface ICountries {
-	[key: string]: [string, string];
-}
-
 declare module 'country-emoji' {
 	/**
 	* Returns the flag emoji of a country.
 	*
-	* @param input - Country code or name.
-	* @returns Flag emoji of the country.
+	* @param {string} input - Country code or name.
+	* @returns {?string} The flag emoji of the country if the provided input is valid.
 	*
 	*/
-	export function flag(input: string): string;
+	export function flag(input: string): string | undefined;
 
 	/**
 	* Returns the code of a country.
 	*
-	* @param input - Flag emoji or name.
-	* @returns Code of the country.
+	* @param {string} input - Flag emoji or name.
+	* @returns {?string} The code of the country if the input is valid.
 	*
 	*/
-	export function code(input: string): string;
+	export function code(input: string): string | undefined;
 
 	/**
 	* Returns the name of a country.
 	*
-	* @param input - Flag emoji or county code.
-	* @returns Name of the country.
+	* @param {string} input - Flag emoji or county code.
+	* @returns {?string} The name of the country if the input is valid.
 	*
 	*/
-	export function name(input: string): string;
+	export function name(input: string): string | undefined;
 
 	/**
 	* Object that contains every single countries' name and language name.
@@ -36,5 +32,7 @@ declare module 'country-emoji' {
 	* @example {"BG": ["Bulgaria", "Bulgarian"]}; countries["BG"] would be ["Bulgaria", "Bulgarian"]
 	*
 	*/
-	export const countries: ICountries;
+	export const countries: {
+		[key: string]: [string, string];
+	};
 }
